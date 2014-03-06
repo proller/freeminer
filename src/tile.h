2bb90353145cc7781f3666d03100df9a1a88f103
proller <proller@github.com>
2014-03-06 21:13:35 +0400

    remove comment

--- src/tile.h
+++ src/tile.h
@@ -147,7 +147,6 @@ public:
 	virtual video::ITexture* getTexture(u32 id)=0;
 	virtual video::ITexture* getTexture(
 			const std::string &name, u32 *id = NULL)=0;
-//	virtual TextureInfo* getTextureInfo(u32 id)=0;
 	virtual IrrlichtDevice* getDevice()=0;
 	virtual bool isKnownSourceImage(const std::string &name)=0;
 	virtual video::ITexture* generateTextureFromMesh(
