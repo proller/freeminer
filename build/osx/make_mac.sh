2d95d2d54b0293edc282f304484bd46e7fa78199
proller <proller@github.com>
2014-12-03 18:03:18 +0300

    fix

--- build/osx/make_mac.sh
+++ build/osx/make_mac.sh
@@ -36,7 +36,7 @@ fi
 
 # Update source code and set version string
 cd freeminer-git
-git checkout osx --force
+git checkout --force osx --
 git pull
 git submodule update --init --recursive
 gitver=`git log -1 --format='%cd.%h' --date=short | tr -d -`
