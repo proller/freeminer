04fbdbd7dec378fa1ca85abce6aa4b8fc80c4e00
proller <proller@github.com>
2016-02-01 00:14:35 +0300

    fix build

--- src/network/fm_connection_sctp.h
+++ src/network/fm_connection_sctp.h
@@ -42,9 +42,9 @@ along with Freeminer.  If not, see <http://www.gnu.org/licenses/>.
 //#include "network/usrsctp/usrsctplib/usrsctp.h"
 #include "../msgpack_fix.h"
 #include "util/msgpack_serialize.h"
-#include "util/thread_pool.h"
-#include "util/concurrent_map.h"
-#include "util/concurrent_unordered_map.h"
+#include "../threading/thread_pool.h"
+#include "../threading/concurrent_map.h"
+#include "../threading/concurrent_unordered_map.h"
 
 #define CHANNEL_COUNT 3
 
